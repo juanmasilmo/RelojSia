300254ab87a150973e6794be952106126a0f6342
Pablo Enrique DIEDRICH <pediedrich@jusmisiones.gov.ar>
2023-05-22 12:57:13 +0000

    Resolve "Esquema Estados"

--- core/env.php
+++ core/env.php
@@ -6,7 +6,7 @@
  */
 $_SESSION['DB_HOST']='localhost';
 $_SESSION['DB_USER']='postgres';
-$_SESSION['DB_PASS']='Sia777';
+$_SESSION['DB_PASS']='postgres';
 $_SESSION['DB_NAME']='reloj2';
 $_SESSION['DB_PORT']='5432';
 $_SESSION['ENTORNO']='LOCAL';
---
+++ modulos/administracion/estados/formulario.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+include("../../../inc/conexion.php");
+conectar();
+
+if($_GET['id']!=0){
+  $sql="SELECT * FROM estados WHERE id =".$_GET['id'];
+  $sql=pg_query($con,$sql);
+  $row=pg_fetch_array($sql);
+}
+
+?>
+<form method="post" id="form" class="needs-validation">
+
+  <input type="hidden" class="form-control" id="id" name="id" value="<?php echo $_GET['id']; ?>">
+
+  <div class="row">
+
+    <div class="col-md-6">
+
+      <label for="descripcion" class="form-label">Descripción
+        <?php if($_GET['id']!=0) echo "[".$row['descripcion']."]";?></label>
+
+      <input type="text" class="form-control" id="descripcion" name="descripcion" required minlength="3"
+        value="<?php if($_GET['id']!=0) echo $row['descripcion'];?>">
+
+      <div class="invalid-feedback">
+        controlar el campo
+      </div>
+    </div>
+
+    <div class="col-md-2">
+
+      <label for="letra" class="form-label">Letra
+        <?php if($_GET['id']!=0) echo "[".$row['letra']."]";?></label>
+
+      <input type="text" class="form-control" id="letra" name="letra" required minlength="3"
+        value="<?php if($_GET['id']!=0) echo $row['letra'];?>">
+
+      <div class="invalid-feedback">
+        controlar el campo
+      </div>
+    </div>
+
+    <div class="col-md-3">
+      <label for=" color" class="form-label">Color</label>
+      <div class=" col-md-4" style="baground:<?php if($_GET['id']!=0) echo $row['color'];?>">
+        <input type="color" class="form-control" id="color" name="color"
+          value="<?php if($_GET['id']!=0) echo $row['color'];?>" required>
+      </div>
+      <div class="invalid-feedback">
+        controlar el campo
+      </div>
+    </div>
+  </div>
+
+</form>
+<div class="mt-4" align="center">
+  <button type="submit" class="btn btn-primary" onclick="guardar()">Guardar</button>
+  <button type="button" class="btn btn-danger" onclick="cerrar_formulario()">Cancelar</button>
+</div>
+
+<hr>
