4609e58df648c761158e0583c06907f13bb46ecf
Pablo Enrique DIEDRICH <pediedrich@jusmisiones.gov.ar>
2023-09-12 13:43:26 +0000

    Resolve "Sistema ReAD"

--- modulos/administracion/carga_novedades/listado.php
+++ modulos/administracion/carga_novedades/listado.php
@@ -3,6 +3,37 @@ session_start();
 include("../../../inc/conexion.php");
 conectar();
 
+
+$id_usuario = $_SESSION['userid'];
+
+$sql_dep = "SELECT id_dependencia FROM usuario_dependencias WHERE id_usuario = $id_usuario";
+$rs_dep = pg_query($con, $sql_dep);
+$res_dep = pg_fetch_all($rs_dep);
+
+$where = '';
+if(pg_num_rows($rs_dep) > 0) {
+
+  $ids_dep = '';
+  for ($i = 0; $i < count($res_dep); $i++) {
+    if(!isset($res_dep[$i+1])){
+      $ids_dep .= $res_dep[$i]['id_dependencia'];
+    }else{
+      $ids_dep .= $res_dep[$i]['id_dependencia'].',';
+    }
+  }
+
+  $where = ' WHERE id IN (' . $ids_dep . ')';
+}
+/**
+ * Dependencias relacionadas al usuario
+ */
+$sql_dependencia = "SELECT id
+                    ,CONCAT(descripcion, ' (', (SELECT descripcion FROM localidades WHERE id = id_localidad),')') as dep
+            FROM dependencias $where";
+$rs_dependencia = pg_query($con, $sql_dependencia);
+$res_dependencia = pg_fetch_all($rs_dependencia);
+
+
 /**
  * Anios
  */
@@ -49,14 +80,29 @@ $hoy = $dt->format('Y-m-d');
 
         <div class="row">
 
-          <!-- Dependencias -->
-          <div class="col-md-6">
-            <label for="">Dependencia</label>
-            <input type="text" class="form-control" placeholder="buscar dependencia ..." name="dependencia"
-              id="dependencia" size="60" onKeyUp="listar_dependencia(this.value);" value="" />
-            <div id="lista_dependencia"></div>
-          </div>
+        <!-- Dependencias -->
 
+        <?php if(pg_num_rows($rs_dep) > 0){ ?>
+            <div class="col-md-6">
+              <label for="">Dependencia</label>
+              <select class="form-control" onchange="listado_agentes(this.value)" name="id_dependencia" id="id_dependencia">
+                <option value="0">Seleccionar ....</option>
+                <?php foreach ($res_dependencia as $value) { ?>
+                  <option value="<?php echo $value['id'] ?>"><?php echo $value['dep'] ?></option>  
+                <?php } ?>
+              </select>
+            </div>
+          <?php }else{ ?>
+            <div class="col-md-6">
+              <label for="">Dependencia</label>
+              <input type="text" class="form-control" placeholder="buscar dependencia ..." name="dependencia"
+                id="dependencia" size="60" onKeyUp="listar_dependencia(this.value);" value="" />
+              <div id="lista_dependencia"></div>
+            </div>
+
+          <?php } ?>
+
+         
           <!-- Filtro DATE -->
           <div class="col-md-2">
             <label for="">Mes</label>
