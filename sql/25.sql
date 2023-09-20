d9a4fd51ac4df4e97965c4ce92c869330d64d7a4
pc_office <pablo@office.com>
2023-09-20 12:03:33 -0300

    *Se quita el esquema "public." del sql 25

--- sql/25.sql
+++ sql/25.sql
@@ -1,4 +1,4 @@
 -- inserto 
-INSERT INTO public.articulos(id_leu, nro_articulo, inciso, descripcion, cantidad_mensual, cantitad_anual, observacion, inasistencias, licencias, retiro, tardanza, excluye_feria, 
+INSERT INTO articulos(id_leu, nro_articulo, inciso, descripcion, cantidad_mensual, cantitad_anual, observacion, inasistencias, licencias, retiro, tardanza, excluye_feria, 
                                 tipo_licencias, sin_fecha_fin, estado, cobra_presentismo, usuario_abm)
     VALUES (null, 'FP', '', 'Firma Planilla', null, null, '', 0, 0, 0, 0, 0, 'ORDINARIAS', 0, 1, 0, 'admin');
