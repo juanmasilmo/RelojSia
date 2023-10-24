4db71139f0538262b757a672ef4aeaf1cf48a6a9
Oscar <oedavids@jusmisiones.gov.ar>
2023-10-24 11:14:25 -0300

    se corrige sincronizar leu para poder sincronizar solo con la fecha desde

--- modulos/administracion/sincronizar_leu/funciones.js
+++ modulos/administracion/sincronizar_leu/funciones.js
@@ -7,20 +7,22 @@ function actualizar(){
 
   if ($("#fecha_desde").val().length < 1) {
     $("#fecha_desde").focus();          
-    return 0;
+   
+    $("#mensaje").html('<div class="alert alert-warning alert-dismissable"> <button type="button" class="close" data-dismiss="alert">&times;</button> <i class="glyphicon glyphicon-ok-sign"></i> <strong>¡ADVERTENCIA!</strong> la sincronización no puede ser mayor a 31 días</div>');;
+    $('#mensaje').fadeIn('slow');  
   }  
 
-  if ($("#fecha_hasta").val().length < 1) {
+  /*if ($("#fecha_hasta").val().length < 1) {
     $("#fecha_hasta").focus();          
     return 0;
-  }  
+  } */ 
 
-  var fechaInicio = new Date($("#fecha_desde").val()).getTime();
-  var fechaFin    = new Date($("#fecha_hasta").val()).getTime();
+  // var fechaInicio = new Date($("#fecha_desde").val()).getTime();
+  // var fechaFin    = new Date($("#fecha_hasta").val()).getTime();
 
-  var diff = (fechaFin - fechaInicio)/(1000*60*60*24);
+  // var diff = (fechaFin - fechaInicio)/(1000*60*60*24);
 
-  if(diff <=62){
+  // if(diff <=62){
 
     showLightbox();
     $.post("modulos/administracion/sincronizar_leu/actualizar.php",{fecha_desde:fecha_desde,fecha_hasta:fecha_hasta,},function(dato){
@@ -28,10 +30,7 @@ function actualizar(){
       $('#mensaje').fadeIn('slow');
       hideLightbox();
     });
-  }else{
-    $("#mensaje").html('<div class="alert alert-warning alert-dismissable"> <button type="button" class="close" data-dismiss="alert">&times;</button> <i class="glyphicon glyphicon-ok-sign"></i> <strong>¡ADVERTENCIA!</strong> la sincronización no puede ser mayor a 31 días</div>');;
-    $('#mensaje').fadeIn('slow');
-  }
+  
 }
 
 function log_sincronizar_leu(sql, co){
